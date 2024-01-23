2ddc2114578115fad9e9304db31fcc2f9fffcabb
Bingwa001 <onebingwa7@gmail.com>
2024-01-23 22:30:34 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app= express();
+dotenv.config();
+
+const dbUri = process.env.MONGO_URI || 'mongodb://localhost:27017/MERN-RealEstateX';
+
+mongoose.connect(dbUri);
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB server');
+});
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-}
+  console.log('Server is running on port 3000!');
+});
 
-);
